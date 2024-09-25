f86021b5b24f34ad1a763232b4918945ae395a12
Kataki-Takanashi <dev.ali@abdurraheem.com>
2024-09-25 15:10:12 -0400

    wrote shopping list with 2 items

--- src/main/java/com/pluralsight/ShoppingList.java
+++ src/main/java/com/pluralsight/ShoppingList.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
 
 public class ShoppingList {
+
+    public  static void main(String[] args)
+    {
+        System.out.println("""
+                Shopping List
+                -------------
+                Apples
+                Olives
+                """);
+    }
+
 }
